730880faf694eb0663ebd104e0ac0bd60c795f0d
Loay Al Jahwari <loay.aljahwari@gmail.com>
2021-12-09 18:58:19 +0400

    initialized the header component

---
+++ src/HeaderComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+class HeaderComponent extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Hello World!</p>
+            </div>
+
+
+
+        )
+    }
+
+}
+
+export default HeaderComponent;
