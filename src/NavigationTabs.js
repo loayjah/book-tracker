b70e011b401a09707f29e68d241bd4d64939b082
Loay Al Jahwari <loay.aljahwari@gmail.com>
2021-12-11 12:17:30 +0400

    initialized component class

---
+++ src/NavigationTabs.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+class NavigationTabs extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
