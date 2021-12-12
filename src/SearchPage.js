8045660212e6beed6a628884b9f172c43873e5fc
Loay Al Jahwari <loay.aljahwari@gmail.com>
2021-12-12 23:38:12 +0400

    added search box

---
+++ src/SearchPage.js
@@ -0,0 +1,20 @@
+import HeaderComponent from './HeaderComponent'
+import React, { Component } from 'react';
+import ListBooks from './ListBooks';
+
+
+class SearchPage extends Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <HeaderComponent />
+                <div class="search-container">
+                    <input class="search-input" type="text" placeholder="Search..."/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchPage;
